a6c8189fcae976296e7de53e90d01fdfc2072c5e
Abhishrek05 <90010729+Abhishrek05@users.noreply.github.com>
2024-01-19 21:24:11 -0700

    intake boilerplate code (im tweakin)

---
+++ src/main/java/frc4388/robot/subsystems/Intake.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc4388.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkLimitSwitch;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  private CANSparkMax intakeMotor;
+  public Intake(CANSparkMax intakeMotor) {
+    this.intakeMotor = intakeMotor;
+    
+  }
+
+  public void spinIntakeMotor() {
+    intakeMotor.set(0.2);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
