5ceca118c72dbe935fb7f1e2beb76b694f492146
Abhishrek05 <90010729+Abhishrek05@users.noreply.github.com>
2024-02-07 18:53:57 -0700

    created a PID for the pivot (im geekin)

--- src/main/java/frc4388/robot/subsystems/Intake.java
+++ src/main/java/frc4388/robot/subsystems/Intake.java
@@ -6,21 +6,43 @@ package frc4388.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkLimitSwitch;
+import com.revrobotics.SparkPIDController;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc4388.robot.Constants;
 import frc4388.robot.Constants.IntakeConstants;
+import frc4388.robot.commands.PID;
+import frc4388.utility.Gains;
 
 public class Intake extends SubsystemBase {
   
   private CANSparkMax intakeMotor;
   private CANSparkMax pivot;
+  private SparkPIDController m_spedController;
+  private static Gains armGains = IntakeConstants.ArmPID.INTAKE_GAINS;
+  private SparkLimitSwitch forwardLimit;
+  private SparkLimitSwitch reverseLimit;
 
   /** Creates a new Intake. */
   public Intake(CANSparkMax intakeMotor, CANSparkMax pivot) {
     this.intakeMotor = intakeMotor;
     this.pivot = pivot;
+
+    pivot.restoreFactoryDefaults();
+
+    forwardLimit = pivot.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
+    reverseLimit = pivot.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
+    forwardLimit.enableLimitSwitch(true);
+    reverseLimit.enableLimitSwitch(true);
+    
+
+    //Arm PID
+    m_spedController = intakeMotor.getPIDController();
+    m_spedController.setP(armGains.kP);
+    m_spedController.setI(armGains.kI);
+    m_spedController.setD(armGains.kD);
   }
 
   //hanoff
