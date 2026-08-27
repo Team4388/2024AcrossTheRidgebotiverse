9f9db838c8b2611600de7260aa6ff46beb2cbb6e
mimigamin <mira.j.maroni@gmail.com>
2026-08-26 21:47:39 -0600

    Updating files and adding 26.2.1
    Can't update vendors???

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024AcrossTheRidgebotiverse";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "80898f9b8989798e047c7ebbc145dd2aa7d4f12e";
-  public static final String GIT_DATE = "2026-04-28 16:32:28 MDT";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "58384c7d6eb084d6d1df2faf40475219664086a5";
+  public static final String GIT_DATE = "2026-04-28 17:52:23 MDT";
   public static final String GIT_BRANCH = "2026-postseason-demo";
-  public static final String BUILD_DATE = "2026-04-28 17:50:07 MDT";
-  public static final long BUILD_UNIX_TIME = 1777420207150L;
+  public static final String BUILD_DATE = "2026-08-26 21:47:22 MDT";
+  public static final long BUILD_UNIX_TIME = 1787802442744L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
